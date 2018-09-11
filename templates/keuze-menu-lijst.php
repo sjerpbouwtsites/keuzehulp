9cfd907583c75e9799dd3fba13872a17ddcb1461
Sjerp van Wouden <info@sjerpbouwtsites.nl>
2018-09-11 09:34:07 +0200

    HTML dynamische inhoud keuzestappenmenu

---
+++ templates/keuze-menu-lijst.php
@@ -0,0 +1,13 @@
+<aside class='keuze-menu-lijst'>
+	<header>
+		<img src='/wp-content/plugins/efiber/iconen-nieuw/svg/keuzes.svg' alt='schuiven' height='30' width='30' />
+		<h2>Uw keuzes</h2>
+	</header>
+	<nav class='stappen-links'>
+		<ul>
+			<li>
+				
+			</li>
+		</ul>
+	</nav>
+</aside>

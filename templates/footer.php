1144cc3eca657fa0eda5cbb9b6a6fe1fccefa9fc
Sjerp van Wouden <info@sjerpbouwtsites.nl>
2018-09-11 14:03:51 +0200

    footer afgesplitst en thema footer onmogelijk gemaakt

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 </main>
 <footer class='app-footer' role='contentinfo'>
 	<div class='verpakking'>
-		<span>&copy;<?= date("Y"); ?> <?=get_field('bedrijfsnaam', 'option')?><br>Realisatie: <a target="_blank" href="https://indrukwekkend.nl">indrukwekkend.nl</a></span>
+		<span>&copy;<?= date("Y"); ?> <?=get_field('bedrijfsnaam', 'option')?><br>Realisatie: <a target="_blank" href="https://indrukwekkend.nl">Indrukwekkend.nl</a></span>
 	</div>
 </footer>
 <?php 
